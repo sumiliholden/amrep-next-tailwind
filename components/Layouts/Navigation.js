fa79bab307a38d2d0cf09e47a8c0635b8c5fbabb
Bayani Catao <tsitonggwapo@gmail.com>
2022-02-16 12:37:56 +0800

    inayab auth0

--- components/Layouts/Navigation.js
+++ components/Layouts/Navigation.js
@@ -145,7 +145,7 @@ export const Navigation = ( ) => {
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <a
-              href="#"
+              href="/api/auth/login"
               className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
             >
               Sign in
