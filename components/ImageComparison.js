ea6b609d3bb2838deee0103916b7455263f56d1d
sumiliholden <sumiliholden@gmail.com>
2022-02-19 10:27:55 +0800

    added imageComparison function

---
+++ components/ImageComparison.js
@@ -0,0 +1,93 @@
+import Layout from "../layouts/Layout";
+import { useState, useRef, TouchEvent } from "react";
+
+export default function Example() {
+  const [imageRevealFraq, setImageRevealFraq] = useState(0.5);
+  const imageContainer = useRef(undefined);
+
+  const slide = (xPosition) => {
+    const containerBoundingRect =
+      imageContainer.current.getBoundingClientRect();
+
+    setImageRevealFraq(() => {
+      if (xPosition < containerBoundingRect.left) {
+        return 0;
+      } else if (xPosition > containerBoundingRect.right) {
+        return 1;
+      } else {
+        return (
+          (xPosition - containerBoundingRect.left) / containerBoundingRect.width
+        );
+      }
+    });
+  };
+  const handleTouchMove = (event = TouchEvent) => {
+    slide(event.touches.item(0).clientX);
+  };
+  const handleMouseDown = () => {
+    window.onmousemove = handleMouseMove;
+    window.onmouseup = handleMouseUp;
+  };
+  const handleMouseMove = (event = MouseEvent) => {
+    slide(event.clientX);
+  };
+  const handleMouseUp = () => {
+    window.onmousemove = undefined;
+    window.onmouseup = undefined;
+  };
+  return (
+    <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8 ">
+      <div
+        ref={imageContainer}
+        className="overflow-hidden relative select-none group"
+      >
+        <img
+          src="https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg"
+          alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
+          className="pointer-events-none"
+        />
+        
+        <img
+          style={{
+            filter: "grayscale(100%)",
+            clipPath: `polygon(0 0, ${imageRevealFraq * 100}% 0, ${
+              imageRevealFraq * 100
+            }% 100%, 0 100%)`,
+          }}
+          src="https://tailwindui.com/img/ecommerce-images/home-page-03-featured-category.jpg"
+          alt="Two models wearing women's black cotton crewneck tee and off-white cotton crewneck tee."
+          className="absolute inset-0 pointer-events-none"
+        />
+        <div
+          style={{ left: `${imageRevealFraq * 100}%` }}
+          className="absolute inset-y-0 group-hover:opacity-100 md:opacity-0"
+        >
+          <div className="relative h-full opacity-50 hover:opacity-100">
+            <div className="absolute inset-y-0 bg-white w-0.5 -ml-px "></div>
+            <div
+              style={{ touchAction: "none" }}
+              onMouseDown={handleMouseDown}
+              onTouchMove={handleTouchMove}
+              className="h-12 w-12 -ml-6 -mt-6 rounded-full bg-white absolute top-1/2 shadow-xl flex items-center justify-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 tex-gray-400 rotate-90 transform cursor-pointer"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 9l4-4 4 4m0 6l-4 4-4-4"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/showcase.js
+++ pages/showcase.js
@@ -1,21 +1,12 @@
-
-//components
 import Layout from "../layouts/Layout";
-import { PricingPlanComponent } from "../components/sections/PricingPlanComponent";
-import { LogoCloudComponentOnBrand } from "../components/sections/LogoCloudComponent";
+import ImageComparison from "../components/ImageComparison";
 
-const title = 'Packages'
-export default function Packages() {
+export default function Showcase() {
   return (
-    <>
-      <Layout>
-        <div className="bg-white">
-          {/* Pricing with four tiers and toggle */}
-          <PricingPlanComponent title={title}/>
-          {/* Logo cloud on brand */}
-          <LogoCloudComponentOnBrand />
-        </div>
-      </Layout>
-    </>
+    <Layout>
+      <div className="bg-gray-50">
+        <ImageComparison />
+      </div>
+    </Layout>
   );
 }
